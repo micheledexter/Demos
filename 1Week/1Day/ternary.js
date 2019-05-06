ec3f881491e8975a1f5bed69b396f4494eddaeef
Shely Dexter <micheleannedexter@gmail.com>
2019-05-06 16:21:07 -0400

    ternary

---
+++ 1Week/1Day/ternary.js
@@ -0,0 +1,8 @@
+// ternary is short hand for an if else statement
+
+(false) ? console.log('true') : console.log('false');
+
+// DON'T NEST TERNARY
+(false) ? console.log('true') :
+    (false) ? console.log('true') :
+        (false) ? console.log('true') : console.log('false');
