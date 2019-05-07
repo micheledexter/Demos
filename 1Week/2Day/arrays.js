7881746ff833e43bfc684a6805bfe3af9587f3c3
Shely Dexter <micheleannedexter@gmail.com>
2019-05-07 15:30:29 -0400

    arrays

---
+++ 1Week/2Day/arrays.js
@@ -0,0 +1,45 @@
+// an array in JS is a collection of values
+let arr = [
+    'Alec',
+    'Ralph',
+    'Eva',
+    42,
+    {
+        airbags: {
+            left: true,
+            right: true
+        }
+    }
+];
+
+console.log(arr);
+
+console.log(arr[4]);
+console.log(arr[6]);
+
+arr.push('this goes to the end');
+arr.unshift('this goes to the beginning');
+
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
+
+// enhanced for loop, also known as the 'for of' loop
+for (let key of arr) {
+    console.log(key);
+}
+
+console.log(arr);
+
+let matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+
+console.log(matrix[0]);
+console.log(matrix[1][1]);
+
+console.log(arr.slice(2, 4));
+console.log(arr.splice(1, 3));
+console.log(arr);
