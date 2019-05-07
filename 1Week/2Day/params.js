c80417c77bee76f2c041dc79449e5c44fe78420b
Shely Dexter <micheleannedexter@gmail.com>
2019-05-07 15:10:14 -0400

    params

--- 1Week/2Day/params.js
+++ 1Week/2Day/params.js
@@ -1,3 +1,12 @@
-function doMath(a, b) {
-    return a + b;
+function doMath(a, b = 10, ...params) {
+    console.log(a + b);
+    let x = 0;
+    for(let i = 0; i < params.length; i++) {
+        x += params[i];
+    }
+    if (params.length > 0) console.log(params, x);
 }
+
+doMath(1, 2);
+doMath(1);
+doMath(1, 2, 3, 4, 5, 6);
