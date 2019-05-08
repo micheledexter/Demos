e5af5807294cf575440cdad2ab16169458452ff9
Shely Dexter <micheleannedexter@gmail.com>
2019-05-08 15:02:39 -0400

    destructuring

---
+++ 1Week/3Day/destructuring.js
@@ -0,0 +1,19 @@
+let animal = {
+    type: {
+        family: 'dinosaur',
+        species: 'Velociraptor'
+    },
+    weight: 'enough',
+    numOfTeeth: 1000,
+    flyable: 'maybe?',
+    carnivore: true
+}
+
+let {carnivore, flyable} = animal;
+
+console.log(`carnivore is ${carnivore} and flyable is ${flyable}`);
+
+let {type} = animal;
+type.footprint = 'pretty big';
+
+console.log(animal.type);
